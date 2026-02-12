473d6c69007c4eb804dfda2a3374682bc5f8e3c2
dravenlewis <dravenlewis.is@outlook.com>
2026-02-12 07:17:28 -0600

    Changed Main, and its Point Lights that are the issue not Spot.

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,7 +6,7 @@ int main(){
 
     DisplayMode mode = DisplayMode::New(1280,720);
     mode.resizable = true;
-    GameEngine engine(mode, "Modern OpenGL 4 - Render Thingy");
+    GameEngine engine(mode, "Modern OpenGL 4 - Render Engine - Editor");
 
     auto demoFactory = [](RenderWindow* window) -> PScene {
         return std::make_shared<DemoScene>(window);
